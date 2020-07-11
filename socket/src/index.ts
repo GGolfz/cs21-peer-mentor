19e63cbb3f507621d7f87c8811c5129a4932951f
GGolfz <wisarutgolf11@gmail.com>
2020-07-11 18:16:03 +0700

    [ADD] Demo chat room

--- socket/src/index.ts
+++ socket/src/index.ts
@@ -1,20 +1,36 @@
 import {app} from './app'
 import { Server } from 'http'
+import { Socket } from 'socket.io'
 const port = process.env.PORT || 5000
 
 
 const server = app.listen(port, () => {
 	console.log(`Running on ${port}`)
 })
-const io:Server = require('socket.io').listen(server)
+const io:Socket = require('socket.io').listen(server)
+var user:String =""
 io.on('connection', socket => {
-    console.log('user connected')
-    socket.on('sent-message', (data:string) => {
-      console.log(data)
-      io.emit('new-message',data)
-    })
+  console.log('user connected')
+  socket.on('joinRoom', (data: { room: any; username: any }) => {
+    const {room,username} = data
+    user = username
+    console.log(`${username} join ${room}`)
+    socket.join(room);
+  });
 
-    socket.on('disconnect', () => {
-        console.log('user disconnected')
-    })
-  })
+  socket.on('chatMessage', (data: { username: any; message: any; room: any }) => {
+    const {username,message,room} = data
+    console.log(data)
+    io.to(room).emit('message', `${username}: ${message}`);
+    const noti = {roomID:room,username,message}
+    io.emit('notify',noti)
+  });
+
+  // Runs when client disconnects
+  socket.on('disconnect', () => {
+    if (user.length > 0 ){
+      console.log(`${user} disconnected`)  
+    }
+  }
+  );
+});
