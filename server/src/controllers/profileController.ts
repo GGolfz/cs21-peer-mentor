a3e4ccfbc42956121b5b99408799ba1c5e5f940f
Sethanant Pipatpakorn <sethanant.p@icloud.com>
2020-07-12 14:40:06 +0700

    [ADD] Sharp to optimize the image before upload to GCS

--- server/src/controllers/profileController.ts
+++ server/src/controllers/profileController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express'
 import { User } from '../models/user'
 import { Storage } from '@google-cloud/storage'
 import fs from 'fs'
+import sharp from 'sharp'
 
 export const getProfileController = async (req: Request, res: Response): Promise<void> => {
 	const userProfile = await User.findOne({ student_id: req.user })
@@ -9,30 +10,47 @@ export const getProfileController = async (req: Request, res: Response): Promise
 }
 
 export const newProfilePicController = async (req: Request, res: Response): Promise<void> => {
+	// Verify the existing of a image file
 	if (!req.file) {
 		res.status(400).send({ error: 'Profile picture is not included' })
 		return
 	}
-	const filetype = /(\.\w*)/.exec(req.file.originalname)
+	const originalFilePath = `uploads/${req.file.filename}`
+	const optimizedFilePath = `${originalFilePath}_1`
 
-	if (filetype == null) {
-		res.status(400).send({ error: 'File types is not specify' })
+	const filetypeTest = /.*\.(jpeg|jpg|png)/.test(req.file.originalname)
+
+	if (filetypeTest == false) {
+		fs.unlinkSync(originalFilePath)
+		res.status(400).send({ error: 'File types is not valid' })
 		return
 	}
-	// const student_id = req.user
-	const student_id = 62130500230 // Placeholder
+	try {
+		// Optimize the image
+		const imageBuffer = await sharp(originalFilePath)
+			.resize({
+				height: 200,
+				width: 200,
+			})
+			.jpeg()
+			.toFile(optimizedFilePath)
 
-	// Upload to Google Cloud Storage
-	const storage = new Storage({ keyFilename: 'GCS-service-account.json' })
-	const bucketName = 'cs21-peer-mentor'
-	const filepath = `uploads/${req.file.filename}`
+		// const student_id = req.user
+		const student_id = 62130500230 // Placeholder
 
-	await storage.bucket(bucketName).upload(filepath, {
-		destination: `profile_img/${student_id}${filetype[0]}`,
-		gzip: true,
-	})
+		// Upload to Google Cloud Storage
+		const storage = new Storage({ keyFilename: 'GCS-service-account.json' })
+		const bucketName = 'cs21-peer-mentor'
+		await storage.bucket(bucketName).upload(optimizedFilePath, {
+			destination: `profile_img/${student_id}.jpeg`,
+			gzip: true,
+		})
 
-	res.status(201).send({ success: true })
-
-	fs.unlinkSync(filepath)
+		res.status(201).send({ success: true })
+	} catch (err) {
+		res.status(500).send()
+	}
+	// Clean up the temp file
+	fs.unlinkSync(originalFilePath)
+	fs.unlinkSync(optimizedFilePath)
 }
