79eab2d5a680227a0e3635ce8618f07ddbc291d8
fantasyzernontra <fantasyzernontra@hotmail.com>
2020-07-12 17:37:30 +0700

    [FIX] Duplicate File

--- server/src/routers/profileRoute 2.ts
+++
@@ -1,7 +0,0 @@
-import { Router } from 'express'
-import { verifyAuth } from '../controllers/authController'
-import { getProfileController } from '../controllers/profileController'
-
-export const profileRoute = Router()
-
-profileRoute.get('/profile', verifyAuth, getProfileController)
