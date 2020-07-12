d68b0e882a08b3a64e2063a3f4fb2d22b76d8802
Sethanant Pipatpakorn <sethanant.p@icloud.com>
2020-07-12 14:15:45 +0700

    [ADD] Route controller for recieving image from multipart/formdata

--- server/src/controllers/profileController.ts
+++ server/src/controllers/profileController.ts
@@ -1,7 +1,38 @@
 import { Request, Response } from 'express'
 import { User } from '../models/user'
+import { Storage } from '@google-cloud/storage'
+import fs from 'fs'
 
 export const getProfileController = async (req: Request, res: Response): Promise<void> => {
 	const userProfile = await User.findOne({ student_id: req.user })
 	res.send(userProfile)
+}
+
+export const newProfilePicController = async (req: Request, res: Response): Promise<void> => {
+	if (!req.file) {
+		res.status(400).send({ error: 'Profile picture is not included' })
+		return
+	}
+	const filetype = /(\.\w*)/.exec(req.file.originalname)
+
+	if (filetype == null) {
+		res.status(400).send({ error: 'File types is not specify' })
+		return
+	}
+	// const student_id = req.user
+	const student_id = 62130500230 // Placeholder
+
+	// Upload to Google Cloud Storage
+	const storage = new Storage({ keyFilename: 'GCS-service-account.json' })
+	const bucketName = 'cs21-peer-mentor'
+	const filepath = `uploads/${req.file.filename}`
+
+	await storage.bucket(bucketName).upload(filepath, {
+		destination: `profile_img/${student_id}${filetype[0]}`,
+		gzip: true,
+	})
+
+	res.status(201).send({ success: true })
+
+	fs.unlinkSync(filepath)
 }
--- server/src/routers/profileRoute.ts
+++ server/src/routers/profileRoute.ts
@@ -1,7 +1,12 @@
 import { Router } from 'express'
 import { verifyAuth } from '../controllers/authController'
-import { getProfileController } from '../controllers/profileController'
+import { getProfileController, newProfilePicController } from '../controllers/profileController'
+import multer from 'multer'
+
+const upload = multer({ dest: 'uploads/' })
 
 export const profileRoute = Router()
 
 profileRoute.get('/profile', verifyAuth, getProfileController)
+
+profileRoute.post('/profilepic', upload.single('profile_pic'), newProfilePicController)
