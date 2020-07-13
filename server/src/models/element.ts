1c57804bcab7eb1d7f7356e7a9ad59b9d450fe4d
Sethanant Pipatpakorn <sethanant.p@icloud.com>
2020-07-13 12:03:38 +0700

    [ADD] Thai-name to element schema

--- server/src/models/element.ts
+++ server/src/models/element.ts
@@ -2,7 +2,8 @@ import { Schema, model } from 'mongoose'
 
 const elementSchema = new Schema({
 	name: String,
-	image_url: String
+	thai_name: String,
+	image_url: String,
 })
 
 export const Element = model('element', elementSchema)
