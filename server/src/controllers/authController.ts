df007b3f4ac593c8c0d240c8efb0534191f0a0af
Sethanant Pipatpakorn <sethanant.p@icloud.com>
2020-07-13 13:13:52 +0700

    Move callback func of passport to authController

--- server/src/controllers/authController.ts
+++ server/src/controllers/authController.ts
@@ -21,3 +21,47 @@ export const verifyAuth = async (req: Request, res: Response, next: NextFunction
 	}
 	next()
 }
+
+export const passportCallback = async (accessToken: String, refreshToken: String, profile: any, done: Function) => {
+	if (profile.name.familyName != 'SIT-STUDENT') {
+		const error = 'Please sign-in using @ad.sit.kmutt.ac.th'
+		done(error, null)
+		return
+	}
+	const existingUser = await User.findOne({
+		student_id: profile.name.givenName,
+	})
+
+	if (!existingUser) {
+		// TODOS
+		// Add element
+		// Test badges
+		await User.create({
+			student_id: profile.name.givenName,
+			email: profile.emails[0].value,
+			year: determineYear(profile.name.givenName),
+			name: '',
+			display_name: '',
+			bio: '',
+			profile_img: '',
+		})
+	}
+
+	done(null, profile.name.givenName)
+}
+
+const determineYear = (id: String): String => {
+	const year = id.substring(0, 2)
+	switch (year) {
+		case '63':
+			return '1'
+		case '62':
+			return '2'
+		case '61':
+			return '3'
+		case '60':
+			return '4'
+		default:
+			return 'Error'
+	}
+}
