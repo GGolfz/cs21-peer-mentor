838c27e3e3a3a8d539e609d827a84046c9e92dc4
Sethanant Pipatpakorn <sethanant.p@icloud.com>
2020-07-14 11:52:03 +0700

    [FIX] element, profile_img not show

--- server/src/controllers/profileController.ts
+++ server/src/controllers/profileController.ts
@@ -7,14 +7,13 @@ import sharp from 'sharp'
 export const getProfileController = async (req: Request, res: Response): Promise<void> => {
 	const student_id = req.user
 	const userProfile = await User.findOne({ student_id }).populate('element')
-	console.log(userProfile)
 	res.send(toProfileRes(userProfile))
 }
 
 interface profileResBody {
 	display_name: String
 	name: String
-	profile_url: String
+	profile_img: String
 	bio: String
 	year: String
 	element: {
@@ -30,7 +29,6 @@ interface updatedFields {
 }
 
 export const updateProfileController = async (req: Request<{}, {}, updatedFields>, res: Response): Promise<void> => {
-	//Temporarily checking
 	const student_id = req.user
 	if (!req.body.bio && !req.body.display_name) {
 		res.status(400).send({ Error: 'Input is empty. Please includes' })
@@ -46,7 +44,7 @@ export const updateProfileController = async (req: Request<{}, {}, updatedFields
 		{
 			new: true,
 		}
-	)
+	).populate('element')
 	res.send(toProfileRes(profile))
 }
 
@@ -89,21 +87,25 @@ export const newProfilePicController = async (req: Request, res: Response): Prom
 
 		const gcsFilePath = `https://storage.googleapis.com/cs21-peer-mentor/${gcsFile[0].name}`
 		// Write image url to db
-		const profile = await User.findOneAndUpdate({ student_id }, { profile_img: gcsFilePath }, { new: true })
+		const profile = await User.findOneAndUpdate(
+			{ student_id },
+			{ profile_img: gcsFilePath },
+			{ new: true }
+		).populate('element')
 		res.status(201).send(toProfileRes(profile))
 	} catch (err) {
 		res.status(500).send({ error: err })
 	}
 	// Clean up the temp file
-	// fs.unlinkSync(originalFilePath)
-	// fs.unlinkSync(optimizedFilePath)
+	fs.unlinkSync(originalFilePath)
+	fs.unlinkSync(optimizedFilePath)
 }
 
 const toProfileRes = (profile: any): profileResBody => {
 	return {
 		display_name: profile.display_name,
 		name: profile.name,
-		profile_url: profile.profile_url,
+		profile_img: profile.profile_img,
 		bio: profile.bio,
 		year: profile.year,
 		element: {
