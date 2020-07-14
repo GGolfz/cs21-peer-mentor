b157d3420cef8c8a6680338bd8ba41d3ed288bb9
Sethanant Pipatpakorn <sethanant.p@icloud.com>
2020-07-14 14:15:05 +0700

    [ADD] getNewBadgeController

--- server/src/controllers/badgeController.ts
+++ server/src/controllers/badgeController.ts
@@ -1,28 +1,58 @@
 import { Request, Response } from 'express'
 import jwt from 'jsonwebtoken'
 import qr from 'qrcode'
-import sharp from 'sharp'
-import { sign } from 'crypto'
+import { User } from '../models/user'
+import { Element } from '../models/element'
 
+// const sercre = process.env.JWT_SECRET
 const secret = 'somesecret'
 
 export const getQRController = async (req: Request, res: Response): Promise<void> => {
+	// const student_id = req.user
 	const student_id = '62130500230'
 
 	const signedIDToken: String = jwt.sign({ student_id }, secret, {
 		algorithm: 'HS256',
-		expiresIn: '5m',
+		// expiresIn: '5m',
 	})
+	console.log(signedIDToken)
+	// const qrcode = await qr.toFile('uploads/temp.png', `${signedIDToken}`, { type: 'png' })
+}
 
-	const qrcode = await qr.toFile('uploads/temp.png', `${signedIDToken}`, { type: 'png' })
-	// const buffer = await sharp('uploads/temp')
-	// 	.resize({
-	// 		height: 200,
-	// 		width: 200,
-	// 	})
-	// 	.webp()
-	// 	.toBuffer()
-	// console.log(buffer)
-	res.contentType('image/png')
-	res.sendFile('../../uploads/temp.png')
+interface newBadgeReqBody {
+	token: string
+}
+
+export const getNewBadgeController = async (req: Request<{}, {}, newBadgeReqBody>, res: Response): Promise<void> => {
+	// const student_id = req.user
+	const student_id = '62130500230'
+	if (!req.body.token) {
+		res.status(400).send({ error: 'JWT from qrcode is required' })
+		return
+	}
+	// Decode the token
+	const decoded: any = jwt.verify(req.body.token, secret)
+
+	// Get the user and element
+	const user: any = await User.findOne({ student_id }).populate('badges')
+	if (!user) {
+		res.status(404).send({ error: 'User is not found' })
+		return
+	}
+	if (!user.badges) {
+		user.badges = []
+	}
+	const element: any = await Element.findOne({ member: decoded.student_id.substring(9) })
+	if (!element) {
+		res.status(404).send({ error: 'Element is not found' })
+		return
+	}
+	// Check if user aleady own that element badge
+	let ownedElement = user.badges.find((ele: any) => ele._id.toString() == element._id.toString())
+	if (!ownedElement) {
+		user.badges.push(element)
+		await user.save()
+	} // TODO
+	// What to return?
+	res.send(user)
 }
--- server/src/routers/badgeRoute.ts
+++ server/src/routers/badgeRoute.ts
@@ -1,6 +1,9 @@
 import { Router } from 'express'
-import { getQRController } from '../controllers/badgeController'
+import { verifyAuth } from '../controllers/authController'
+import { getQRController, getNewBadgeController } from '../controllers/badgeController'
 
 export const badgeRoute = Router()
 
 badgeRoute.get('/qr', getQRController)
+
+badgeRoute.post('/badge', getNewBadgeController)
