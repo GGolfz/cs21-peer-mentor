dbaffcd134af2943df304f00172096e59f00f0f3
Sethanant Pipatpakorn <the_tkpark@hotmail.com>
2020-07-17 22:47:22 +0700

    [ADD] .env for environment variable

--- client/next.config.js
+++ client/next.config.js
@@ -1,12 +1,7 @@
-
 const withPWA = require('next-pwa')
- 
+
 module.exports = withPWA({
-    pwa: {
-        dest: 'public'
-    },
-    env: {
-        SERVER_URL: 'http://localhost:3050',
-        SOCKET_URL: 'http://localhost:5000'
-    }
+	pwa: {
+		dest: 'public'
+	}
 })
