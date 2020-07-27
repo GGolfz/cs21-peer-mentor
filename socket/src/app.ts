b233165d45e97a885e704b79075a7ba8d0326279
Sethanant Pipatpakorn <sethanant.p@icloud.com>
2020-07-27 09:40:14 +0700

    [ADD] Helmet in socket

--- socket/src/app.ts
+++ socket/src/app.ts
@@ -1,5 +1,7 @@
 import * as express from 'express'
 import * as bodyParser from 'body-parser'
-export const app:express.Application = express.default()
+import helmet from 'helmet'
+export const app: express.Application = express.default()
 
 app.use(bodyParser.json())
+app.use(helmet())
